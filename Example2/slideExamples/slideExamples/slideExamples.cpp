fdd6d824e13ef250d7898b3e822b91272775afeb
Adetunji Adeniran <AAdeniran001@dundee.ac.uk>
2024-04-01 14:34:01 +0100

    Test intLinkedList in main

---
+++ Example2/slideExamples/slideExamples/slideExamples.cpp
@@ -0,0 +1,51 @@
+// slideExamples.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+#include <string>
+#include "intLinkedListS.h"
+#include <cstdlib>
+
+int add(int a, int b)
+{
+    return a + b;
+}
+int subtract(int a, int b)
+{
+    return a - b;
+}
+int main()
+{
+    //int* p;
+    //int x = 34;
+    //p = &x;
+    ////You could access x value with *p
+    //cout << *p << endl;
+    //*p = 44;
+    //cout << x << endl;
+    //x = 99;
+    //cout << *p << endl;
+    
+    //=====Demo: pointer pointing to functions
+   /* int (*op)(int a, int b);
+
+    op = &subtract;
+    cout<<op(5, 2)<<endl;
+    int* p = NULL;*/
+    
+    //======Demo2: Pitfall declaring pointers
+    //int* x, y, z;
+    //int intVar = 34;
+    //int intVar2 = 54;
+    //x = &intVar;
+    //y = intVar2;  //This will not work
+    intLinkedListS* myLinkedList = new intLinkedListS;
+    myLinkedList->addFront(12);
+    myLinkedList->addFront(34);
+    myLinkedList->addFront(45);
+    myLinkedList->addFront(99);
+    myLinkedList->printData();
+    cout << myLinkedList->front() << endl;
+
+}
