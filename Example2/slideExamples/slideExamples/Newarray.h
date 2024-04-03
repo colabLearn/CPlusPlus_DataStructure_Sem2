0f8df33fc52d5d6e38ee0f85875803cf61c56c68
Adetunji Adeniran <AAdeniran001@dundee.ac.uk>
2024-04-03 12:20:44 +0100

    Creating custom array class header --> Newarray.h, using struct arr as array (node/cell)

---
+++ Example2/slideExamples/slideExamples/Newarray.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "gLinkedListS.h"
+
+template <typename T>
+struct arr {
+	T* p;
+	int index;
+	arr<T>* next;
+
+};
+
+template <typename T>
+class Newarray
+{
+private:
+	int min;
+	int max;
+	arr<T>* head;
+public:
+	Newarray(const int min, const int max);
+	bool outOfIndex(const int index);
+	void indexR() const ;
+	arr<T>& arrCell(const int index);
+	void storeAt( int index,  T data);
+	T getAt(const int index);
+	//
+	/*void storeAt(int index, T data);
+	void getAt();*/
+};
+
